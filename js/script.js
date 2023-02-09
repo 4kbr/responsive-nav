6dcf83f989bd5976ed872ed95b8a6bd52ebfd3c1
akbar-human <akbar@hub9.biz>
2023-02-09 13:53:43 +0700

    adding custom css

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 let menu = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
+let rightBar = document.querySelector(".right-bar");
 
 // menu.addEventListener(onclick, (event) => {
 menu.onclick = () => {
   menu.classList.toggle("bx-x");
   navbar.classList.toggle("open");
+  rightBar.classList.toggle("open");
 };
 // });
